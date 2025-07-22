97507fa8ae634ba83b44ffb1f6b0811c23c9d1e9
Hein Htet Win <heinhtetwin386@gmail.com>
2025-07-22 23:44:34 +0700

    fix org ID

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    organization = "org-G2jtpDefU5hMXDP"
-
+    organization = "org-G2jtpDefU5hMXDPD"
     workspaces {
       name = "ace-iac-day-two"
     }
